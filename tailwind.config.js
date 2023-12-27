916843055ab3e00e81d1af9238376eb4592d0cd6
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2023-12-27 22:30:21 +0600

    initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        orange1: "#fe7f36",
+        blue1: "#3693fe",
+      },
+    },
+  },
+  plugins: [],
+};
